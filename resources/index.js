4314eabd7fb475f81da73f5dab6ec1b56574ac2c
Adam Quarles <adamxquarles@gmail.com>
2025-08-06 11:10:17 -0500

    The (actual) initial commit
    What I've worked on so far for my website.

---
+++ resources/index.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.getElementById("mysideNav").style.width = "15%"
+  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+  document.getElementById("mysideNav").style.width = "0"
+}
